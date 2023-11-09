bb25c1092697f9d0426b89ab29f560c569dce90f
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2023-11-09 00:09:42 -0500

    feat(offset): added offset option

--- excel_modifier/__init__.py
+++ excel_modifier/__init__.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import xlsxwriter
 
 
 class ExcelModifier:
@@ -76,7 +75,7 @@ class ExcelModifier:
         self._colourize_columns.
         """
         instruction_split = instructions.split()
-        POSSIBLE_INSTRUCTIONS = 'MmCcps'
+        POSSIBLE_INSTRUCTIONS = 'MmCcpsoO'
 
         params_to_pass = {}
 
@@ -107,6 +106,10 @@ class ExcelModifier:
                                 params_to_pass['formatting_option'] = 'colour_lower'
                             case 'b':
                                 params_to_pass['formatting_option'] = 'colour_both'
+                    case 'o':
+                        params_to_pass['row_offset'] = int(next_item)
+                    case 'O':
+                        params_to_pass['column_offset'] = int(next_item)
 
         return params_to_pass
 
@@ -116,25 +119,15 @@ class ExcelModifier:
         of all sheets in self.sheets_to_modify based on the instructions.
 
         Args:
-        - columns: One of the following:
-            - a list of strings which signify the columns to which the
-              instructions passed to this function should be applied (for every
-              sheet in self.sheets_to_modify). If this is a list, then the
-              "s" argument from the instruction string will be used
-            - a dictionary of the form {column_name: format_option} where
-              column_name is a valid column name in the given sheet and
-              format_option is an item from the set { 'u', 'l', 'b' }. 'u'
-              stands for upper, meaning elements within the upper margin
-              will be colorized, and similarly for lower and both.
-
-              Note that, if this argument is a dict, then the "s" option from
-              the instruction string will be ignored.
+        - columns: a list of strings which signify the columns to which the
+          instructions passed to this function should be applied (for every
+          sheet in self.sheets_to_modify).
         - instructions: A string of instructions that specify how each column
-        should be modified. The specification for this instructions string is
-        found below:
+          should be modified. The specification for this instructions string is
+          found below:
 
         Specification for Instructions String (note: order in which these options are specified
-        does not matter but all of them except 's' must be present in an instructions string):
+        does not matter but all of them must be present in an instructions string):
         - M is used to specify the upper margin percentage, which is the percentage
         of the data in a column to consider as an UPPER margin. It is followed by
         a space and an float to 0.0 to 100.0 (e.g. M 35.0).
@@ -154,25 +147,28 @@ class ExcelModifier:
         - s is used to specify the sections of the columns that must be colorized.
         It is followed by a space and one of {'u', 'l', 'b'}. 'u' stands for upper,
         meaning elements within the upper margin will be colorized, and similarly
-        for lower and both. Note that this argument will be ignored if the type
-        of columns is a dictionary.
+        for lower and both.
+        - o is used to specify the row offset, which is the row number from
+        which to start writing. It is followed by a space and an integer that
+        is greater than or equal to 0. This option is helpful in cases where
+        the data to colourize does not start from row 0 (or row 1 in practice).
+        - o is used to specify the column offset, which is the column number from
+        which to start writing. It is followed by a space and an integer that
+        is greater than or equal to 0. This option is helpful in cases where
+        the data to colourize does not start from column 0 (or column 1 in practice).
 
         EXAMPLE USAGE:
-        Here are 2 example calls:
+        Here is an example call:
 
-        1. colourize_columns(['mean', 'missing'], 'M 5.0 m 10.0 C g c r p 10.0 s b')
+        colourize_columns(['mean', 'missing'], 'M 5.0 m 10.0 C g c r p 10.0 s b o 1 O 0')
 
         This will colourize the 'mean' and 'missing' columns in every sheet in
         self.set_sheets_to_modify, considering the upper margin as 5% and colouring
         it green, the lower margin as 10% and colouring it red, considering the "majority
-        percentage" as 10%, and colourizing both the upper and lower margins. Note that
-        even if integers are used in place of the floats, this function still works.
-
-        2. colourize_columns({'mean': 'u', 'missing': 'l'}, 'M 5.0 m 10.0 C g c r p 10.0')
-
-        This does the same thing as 1, except the 'mean' column only has the upper
-        margin colourized and the 'missing' column only has the lower margin
-        colourized.
+        percentage" as 10%, colourizing both the upper and lower margins, and
+        starting colourization and overwriting from row 1 (to account for the title
+        row) and column 0. Note that even if integers are used in place of the floats,
+        this function still works.
 
         For any further clarification on options, view the docstring of
         self._colourize_columns.
@@ -187,14 +183,17 @@ class ExcelModifier:
         colour_options = (
             parsed_instructions['colour_upper'], parsed_instructions['colour_lower'])
         majority_percentage = parsed_instructions['majority_percentage']
+        write_offsets = (
+            parsed_instructions['row_offset'], parsed_instructions['column_offset'])
 
         # call the helper function
         self._colourize_columns(columns, formatting_option, margin_options,
-                                colour_options, majority_percentage)
+                                colour_options, majority_percentage, write_offsets)
 
     def _colourize_columns(self, columns_to_format: list[str], formatting_option: str,
                            margin_options: tuple[float, float], colour_options:
-                           tuple[str, str] = ('green', 'red'), majority_percentage: float = 10.0) -> None:
+                           tuple[str, str] = ('green', 'red'), majority_percentage: float = 10.0,
+                           write_offsets: tuple[int, int] = (0, 0)) -> None:
         """
         Applies the given column_formatting, colour_options, and margin_options to the
         each sheet in self.sheets_to_modify with self.workbook_writer.
@@ -215,7 +214,12 @@ class ExcelModifier:
           percentage that a specific data point must take up in a column to be
           considered a "majority". If the smallest or largest element in a
           column takes up at least majority_percentage% of the column, then it
-          will not be included in the bound.
+          will not be included in the bound. Default value is 10.
+        - write_offsets: a tuple of the form (M, N) where M is the row number
+          from which to start overwriting and N is the column number from which
+          to start overwriting. This is necessary for correct formatting if the
+          data you want to colourize in initial sheet(s) do not start at row
+          and column 0 and 0. Default value is (0, 0).
 
         EXAMPLE USAGE:
         Assume this ExcelModifier has a workbook_writer that edits the file
@@ -228,6 +232,7 @@ class ExcelModifier:
         - margin_options = (5, 10)
         - colour_options = ('green', 'red')
         - majority_percentage = 10
+        - write_offsets = (1, 1)
 
         Then the following would happen:
         - For the columns in sheet_1 called 'mean' and 'missing', the upper 5
@@ -235,12 +240,24 @@ class ExcelModifier:
             - If the largest element in each column takes up at least
               majority_percentage% of the column, it will not be included in
               the colorization.
+            - Colorized cells will be written starting from row 1 (which is the
+              second row in practice) and column 1 (which is the second column
+              in practice)
         """
         # extracting margins
         upper_bound, lower_bound = margin_options
 
         # exctracting the colours
         upper_colour, lower_colour = colour_options
+
+        # creating reusable formats
+        # TODO: custom colours
+        upper_colour_format = self.workbook_writer.book.add_format(
+            {'bg_color': ('#FFC7CE' if upper_colour == 'red' else '#C6EFCE')})
+        lower_colour_format = self.workbook_writer.book.add_format(
+            {'bg_color': ('#FFC7CE' if lower_colour == 'red' else '#C6EFCE')})
+
+        row_offset, column_offset = write_offsets
 
         # do it for each sheet specified
         for sheet_name, sheet_data in self.sheets_to_modify.items():
@@ -252,13 +269,6 @@ class ExcelModifier:
 
             # PART 2: applying conditional formatting based on the bounds and column
             # formatting
-
-            # creating reusable formats
-            # TODO: custom colours
-            upper_colour_format = self.workbook_writer.book.add_format(
-                {'bg_color': ('#FFC7CE' if upper_colour == 'red' else '#C6EFCE')})
-            lower_colour_format = self.workbook_writer.book.add_format(
-                {'bg_color': ('#FFC7CE' if lower_colour == 'red' else '#C6EFCE')})
 
             worksheet = self.workbook_writer.sheets[sheet_name]
             num_rows = sheet_data.shape[0]
@@ -287,7 +297,7 @@ class ExcelModifier:
                     current_column == smallest).mean() >= ignore_bound_percentage
 
                 # for each row in the column
-                for row_pos in range(1, num_rows + 1):
+                for row_pos in range(row_offset, num_rows + 1):
                     current_data = sheet_data.iat[row_pos - 1, column_pos]
 
                     if current_data != 'nan':
@@ -296,24 +306,24 @@ class ExcelModifier:
                             if upper_bound_majority_exists:  # if this is true, we don't want to include the largest element
                                 if largest > current_data >= bounds[column_name][0]:
                                     # overwriting the cell with the original data and new formatting
-                                    worksheet.write(row_pos, column_pos,
+                                    worksheet.write(row_offset + row_pos - 1, column_offset + column_pos,
                                                     current_data, upper_colour_format)
                             else:
                                 if largest >= current_data >= bounds[column_name][0]:
                                     # overwriting the cell with the original data and new formatting
-                                    worksheet.write(row_pos, column_pos,
+                                    worksheet.write(row_offset + row_pos - 1, column_offset + column_pos,
                                                     current_data, upper_colour_format)
 
                         if formatting_option in {'colour_lower', 'colour_both'}:
                             if lower_bound_majority_exists:  # if this is true, we don't want to include the smallest element
                                 if smallest < current_data <= bounds[column_name][1]:
                                     # overwriting the cell with the original data and new formatting
-                                    worksheet.write(row_pos, column_pos,
+                                    worksheet.write(row_offset + row_pos - 1, column_offset + column_pos,
                                                     current_data, lower_colour_format)
                             else:
                                 if smallest <= current_data <= bounds[column_name][1]:
                                     # overwriting the cell with the original data and new formatting
-                                    worksheet.write(row_pos, column_pos,
+                                    worksheet.write(row_offset + row_pos - 1, column_offset + column_pos,
                                                     current_data, lower_colour_format)
 
     def autofit_sheets(self) -> None:
@@ -342,7 +352,7 @@ if __name__ == "__main__":
     modifier = ExcelModifier(writer)
     modifier.set_sheets_to_modify({'Sheet1': df})
 
-    modifier.colourize_columns(['test1'], 'M 20 m 20 c r C g p 90 s b')
+    modifier.colourize_columns(['test1'], 'M 20 m 20 c r C g p 90 s b o 1 O 0')
     modifier.autofit_sheets()
 
     modifier.close()
